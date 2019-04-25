63b359dfe3fc42c11d55812a1fc0aea3c3b2223d
Bimo Horizon <bimo.horizon@go-jek.com>
2019-04-25 16:57:05 +0700

    Update vendor cookbooks

--- cookbooks/kafka/recipes/burrow_search.rb
+++ cookbooks/kafka/recipes/burrow_search.rb
@@ -6,7 +6,6 @@
 #
 
 require 'json'
-require_relative '../libraries/yggdrasil'
 
 # Keep it simple for now
 node.run_state[cookbook_name] ||= {}
@@ -15,12 +14,8 @@ node.run_state[cookbook_name]['burrow']['zookeeper_clusters'] = node[cookbook_na
 
 # Override zookeeper hosts from yggdrasil, if enabled
 if node[cookbook_name]['yggdrasil']['enabled']
-  yggdrasil_namespace = node[cookbook_name]['yggdrasil']['namespace']
-  yggdrasil_overrides = node[cookbook_name]['yggdrasil']['overrides']
+  config_path = File.join(node[cookbook_name]['yggdrasil']['config_dir'], 'yggdrasil.json')
+  config = JSON.parse(File.read(config_path))
 
-  yggdrasil = Yggdrasil.new(node[cookbook_name]['yggdrasil'])
-  yggdrasil_config = yggdrasil.fetch_configs(yggdrasil_namespace, yggdrasil_overrides)
-
-  config = JSON.parse(yggdrasil_config['zookeeper_hosts'])
-  node.run_state[cookbook_name]['burrow']['zookeeper_clusters'] = config
+  node.run_state[cookbook_name]['burrow']['zookeeper_clusters'] = JSON.parse(config['zookeeper_hosts'])
 end
